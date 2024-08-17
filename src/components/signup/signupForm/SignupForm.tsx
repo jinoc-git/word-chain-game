da46020efd20088b84d295530d88774938ba3b18
jinoc <choiae369@gmail.com>
2024-08-17 21:58:31 +0900

    Feat: 회원가입 폼 기초 기능 추가
    - @hookform/resolver 패키지 추가

--- src/components/signup/signupForm/SignupForm.tsx
+++ src/components/signup/signupForm/SignupForm.tsx
@@ -1,26 +1,61 @@
 'use client';
 
 import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Card, CardBody, CardHeader, Input, Link } from '@nextui-org/react';
 
+import { signupFormSchema } from '@/schema/signupFormSchema';
+
+import type { z } from 'zod';
+
+type SignupFormRegisterInput = z.infer<typeof signupFormSchema>;
+
 const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<SignupFormRegisterInput>({
+    resolver: zodResolver(signupFormSchema),
+    mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<SignupFormRegisterInput> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <Card className="main-layout">
       <CardHeader>
         <h1>회원가입</h1>
       </CardHeader>
       <CardBody>
-        <form className=" space-y-3">
-          <Input type="email" label="이메일" placeholder="example@gamil.com" size="md" />
-          <Input type="password" label="비밀번호" placeholder="비밀번호를 입력해주세요" size="md" />
+        <form onSubmit={handleSubmit(onSubmit)} className=" space-y-3">
           <Input
+            {...register('email')}
+            type="email"
+            label="이메일"
+            placeholder="example@gamil.com"
+            size="md"
+          />
+          <Input
+            {...register('password')}
+            type="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
+            size="md"
+          />
+          <Input
+            {...register('confirmPassword')}
             type="password"
             label="비밀번호 확인"
             placeholder="비밀번호를 다시 입력해주세요"
             size="md"
           />
-          <Button color="primary" isDisabled={true} className="w-full">
+          <Button type="submit" color="primary" isDisabled={!isValid} className="w-full">
             회원가입
           </Button>
         </form>
