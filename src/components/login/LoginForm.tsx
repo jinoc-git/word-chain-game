491a8cfaf6573f3b6d55a08f991935a732aaadcc
jinoc <choiae369@gmail.com>
2024-08-19 01:43:18 +0900

    Feat: 로그인 폼 추가
    - 회원가입 삭제 (닉네임만 입력하면 사용 가능하도록 변경)
    - signin 페이지 삭제, login으로 변경
    - 그에 따른 파일 삭제

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react';
+
+import { loginFormSchema } from '@/schema/loginFormSchema';
+
+import type { z } from 'zod';
+
+type LoginFormInput = z.infer<typeof loginFormSchema>;
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormInput>({
+    resolver: zodResolver(loginFormSchema),
+    mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<LoginFormInput> = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Card className="main-layout">
+      <CardHeader>
+        <h1 className=" font-semibold">닉네임 입력</h1>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)} className=" space-y-3">
+          <p className="text-sm text-red-600">{errors.nickname?.message}</p>
+          <Input
+            {...register('nickname')}
+            type="text"
+            label="닉네임"
+            placeholder="사용할 닉네임을 입력해 주세요"
+            size="md"
+          />
+          <Button type="submit" color="primary" isDisabled={!isValid} className="w-full">
+            로그인
+          </Button>
+        </form>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default LoginForm;
