6b23771b5f667b54614ec688f9600c22c453a849
jinoc <choiae369@gmail.com>
2024-09-06 00:36:20 +0900

    Feat: 로그아웃 버튼 클릭 시 session storage의 데이터도 삭제 추가

--- src/components/footer/logout/Logout.tsx
+++ src/components/footer/logout/Logout.tsx
@@ -1,15 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Button } from '@nextui-org/button';
 import { LogOut } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
+import { AuthContext } from '@/context/authContext';
+
 const Logout = () => {
+  const auth = useContext(AuthContext);
+
+  if (auth === null) throw new Error('need AuthProviders');
+
   const router = useRouter();
 
   const handleLogout = () => {
+    auth.logout();
+
     router.push('/');
   };
 
