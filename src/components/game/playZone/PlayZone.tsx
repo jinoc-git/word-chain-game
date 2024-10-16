3b30d7615e533854d515d06be2efdac26cc045fb
jinoc <choiae369@gmail.com>
2024-10-16 23:00:07 +0900

    Feat: playerStore에 isRoomChief 함수 추가
    - 현재 플레이하는 유저가 방장인지 확인하는 isRoomChief 함수 추가
    - 방장 여부에 따라 ControlGame 컴포넌트 선택 렌더링
    - WordList에 border 추가

--- src/components/game/playZone/PlayZone.tsx
+++ src/components/game/playZone/PlayZone.tsx
@@ -2,16 +2,24 @@
 
 import React from 'react';
 
+import { useAuthState } from '@/store/authStore';
+import { usePlayerActions } from '@/store/playerStore';
+
 import ControlGame from './controlGame/ControlGame';
 import EnterWord from './enterWord/EnterWord';
 import WordList from './wordList/WordList';
 
 const PlayZone = () => {
+  const user = useAuthState();
+  const { isRoomChief } = usePlayerActions();
+
+  const playerIsRoomChief = user !== null && isRoomChief(user);
+
   return (
     <section className="">
       <WordList />
       <EnterWord />
-      <ControlGame />
+      {playerIsRoomChief && <ControlGame />}
     </section>
   );
 };
--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand';
 
 import { socket } from '@/socket/socket';
 
+import type { UserType } from '@/types/auth.type';
+
 export interface PlayerType {
   socketId: string;
   userId: string;
   nickname: string;
-  isRoomChief?: boolean;
+  isRoomChief: boolean;
 }
 
 export interface QuitGameArgs {
@@ -18,31 +20,38 @@ interface Actions {
   initPlayer: (users: PlayerType[]) => void;
   playerObserver: () => void;
   quitGameAndOffObserver: (args: QuitGameArgs) => void;
+  isRoomChief: (user: UserType) => boolean;
 }
 
 interface Store {
-  curPlayer: PlayerType[];
+  curPlayers: PlayerType[];
   actions: Actions;
 }
 
 export const playerStore = create<Store>((set, get) => ({
-  curPlayer: [],
+  curPlayers: [],
   actions: {
-    initPlayer: (users: PlayerType[]) => {
-      set({ curPlayer: users });
+    initPlayer: (users) => {
+      set({ curPlayers: users });
     },
     playerObserver: () => {
-      socket.on('updateUser', (users: PlayerType[]) => {
-        set({ curPlayer: users });
+      socket.on('updateUser', (users) => {
+        set({ curPlayers: users });
       });
     },
-    quitGameAndOffObserver: (args: QuitGameArgs) => {
+    quitGameAndOffObserver: (args) => {
       socket.emit('quitGame', args);
       socket.off('updateUser');
-      set({ curPlayer: [] });
+      set({ curPlayers: [] });
+    },
+    isRoomChief: (user) => {
+      const curPlayers = get().curPlayers;
+      const roomChief = curPlayers.find(({ isRoomChief }) => isRoomChief === true);
+
+      return roomChief?.userId === user.id;
     },
   },
 }));
 
-export const usePlayerState = () => playerStore(({ curPlayer }) => curPlayer);
+export const usePlayerState = () => playerStore(({ curPlayers }) => curPlayers);
 export const usePlayerActions = () => playerStore(({ actions }) => actions);
