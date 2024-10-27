690425d661b8f19c592e9c4dd2b66ed94c1c1f53
jinoc <choiae369@gmail.com>
2024-10-27 23:23:09 +0900

    Fix: ControlGame.tsx의 handleGameState 함수 호출 오류 수정
    - useParams를 통해 gameId를 handleGameState함수의 인자로 추가

--- src/components/game/playZone/controlGame/ControlGame.tsx
+++ src/components/game/playZone/controlGame/ControlGame.tsx
@@ -3,18 +3,28 @@
 import React from 'react';
 
 import { Button } from '@nextui-org/button';
+import { useParams } from 'next/navigation';
 
 import useGame from '@/hooks/useGame';
 
 const ControlGame = () => {
   const { isGameStarted, handleGameState } = useGame();
+  const { gameId } = useParams<{ mode: string; gameId: string }>();
 
   return (
     <div className="flex justify-around w-full mt-[100px]">
-      <Button color="primary" isDisabled={isGameStarted} onClick={() => handleGameState(true)}>
+      <Button
+        color="primary"
+        isDisabled={isGameStarted}
+        onClick={() => handleGameState(true, gameId)}
+      >
         게임 시작
       </Button>
-      <Button color="primary" isDisabled={!isGameStarted} onClick={() => handleGameState(false)}>
+      <Button
+        color="primary"
+        isDisabled={!isGameStarted}
+        onClick={() => handleGameState(false, gameId)}
+      >
         게임 중단
       </Button>
     </div>
