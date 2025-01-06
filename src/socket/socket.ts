d8ddcddc8ba317fd8e98814531372205d48cbfd9
jinoc <choiae369@gmail.com>
2025-01-07 01:19:47 +0900

    Fix: socket 연결 엔드포인트 변경 및 다른 옵션 추가

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,6 +2,10 @@
 
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.NEXT_PUBLIC_GAME_CHAIN_SERVER, {
+export const socket = io(process.env.NEXT_PUBLIC_GAME_CHAIN_SERVER + '/socket.io', {
   withCredentials: true,
+  reconnection: true, // 연결 끊김 시 재연결 시도
+  reconnectionDelay: 1000, // 재연결 시도 간격
+  reconnectionAttempts: 5, // 재연결 시도 횟수
+  transports: ['websocket', 'polling'],
 });
