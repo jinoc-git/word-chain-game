5b187706bb59d0b6919ac7cf4db94a2370c2a0cd
jinoc <choiae369@gmail.com>
2025-01-06 21:59:07 +0900

    Fix: socket 엔드포인트 변경

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,6 +2,6 @@
 
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.NEXT_PUBLIC_GAME_CHAIN_SERVER + '/socket.io', {
+export const socket = io(process.env.NEXT_PUBLIC_GAME_CHAIN_SERVER, {
   withCredentials: true,
 });
