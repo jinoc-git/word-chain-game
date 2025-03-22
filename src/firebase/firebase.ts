5b393a649ecb75cd063e54b8eba007eaf5be70a9
jinoc <choiae369@gmail.com>
2025-03-22 20:38:04 +0900

    chore: dev https 추가
    - env 오타 수정

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FB_BUCKEY,
+  storageBucket: process.env.NEXT_PUBLIC_FB_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING,
   appId: process.env.NEXT_PUBLIC_FB_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT,
