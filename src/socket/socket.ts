99414954451678baf7b997c48116dfece50afcdf
jinoc <choiae369@gmail.com>
2025-03-22 16:11:38 +0900

    fix: 소켓 재연결 시도 false 설정

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 // vercel은 socket.io 서버를 지원x
 // aws 무료 할당량 확인 후 가능하면 aws 적용 예정..
 // aws 배포 시 비용이 많이 발생 => supabase를 사용하여 사용자가 입력한 단어를 유효성 검사 후 저장하는 방식으로 realtime 적용 예정
-export const socket = io();
+export const socket = io({ reconnection: false });
 
 const socketConfig = {
   withCredentials: true,
