50dc50704294abd855fa33b4f97571d6a9a1ef3e
jinoc <choiae369@gmail.com>
2025-03-26 20:35:55 +0900

    feat: 한글로만 이루어져있는지 확인 로직 추가

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -3,12 +3,18 @@ import React from 'react';
 import { checkValidWord } from '@/lib/word';
 import { useWordActions } from '@/store/wordStore';
 import { checkFirstCharacter } from '@/utils/checkFirstCharacter';
+import checkOnlyKorean from '@/utils/checkOnlyKorean';
 
 const useWords = () => {
   const [isValidWord, setIsValidWord] = React.useState(false);
   const { getWordsCount, getLastWord, pushNewWord } = useWordActions();
 
   const enterWordAndCheck = async (enterWord: string) => {
+    const isOnlyKorean = checkOnlyKorean(enterWord);
+    if (!isOnlyKorean) {
+      setIsValidWord(false);
+      return false;
+    }
     if (getWordsCount() >= 1) {
       const lastWord = getLastWord();
       const isValidFirstCharacter = checkFirstCharacter(lastWord, enterWord);
