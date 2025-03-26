8b3ab1cd0726a1084ecba28d4da93578518787c6
jinoc <choiae369@gmail.com>
2025-03-26 22:49:58 +0900

    test: Game.test.tsx 오류 수정

--- __test__/pages/Game.test.tsx
+++ __test__/pages/Game.test.tsx
@@ -1,9 +1,11 @@
 import { render } from '@testing-library/react';
+import { useParams } from 'next/navigation';
 
 import Game from '@/app/game/[mode]/[gameId]/page';
 
 describe('Game', () => {
   it('should render', () => {
+    vi.mocked(useParams).mockReturnValue({ mode: 'multi', gameId: 'abcdef' });
     render(<Game />);
 
     // const a = screen;
